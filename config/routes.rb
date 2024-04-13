e5da8e58825f539b648d11c9c06a4c3bd16363af
Mitsuki Yanagi <iganaymjs2@gmail.com>
2024-04-13 23:43:33 +0900

    fix:cardsリソースへのリクエストを処理するコントローラをCardsコントローラからEntriesコントローラに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'entries/index'
   get 'entries/show'
-  resources :cards
+  resources :cards, :controller => entries
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
